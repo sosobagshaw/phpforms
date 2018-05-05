6143028c000b07da750661989e2c415dd2c5de31
Bagshaw <sosobagshaw@gmail.com>
2018-05-05 20:40:08 +0100

    adding files

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+ $name = $_GET('name');
+ $email = $_GET('email');
+ $address = $_GET('address');
+
+ echo "Welcome:".$name;
+ echo "<br/>";
+ echo "Please varify that email address is: ".$email;
+ echo "<br/>and that our address is: ".$address;
+ ?>
